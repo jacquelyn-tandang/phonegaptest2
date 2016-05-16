2e3ad327575ed4e4a8e7c09a1492ca86a9ecd3e5
Jackie <jacquelyn.arroyo-tandang@ima-mt.com>
2016-05-17 06:42:46 +0800

    FB Settings Changes

--- www/js/config.js
+++ www/js/config.js
@@ -2,13 +2,12 @@
 localStorage.setItem("application_env", "production"); //test or production
 
 localStorage.setItem("_api_server", "http://188.166.215.70:82");
-localStorage.setItem("_domain", "188.166.215.70:81");
+localStorage.setItem("_domain", "sarai.ph:81");
 localStorage.setItem("_api_key", "456");
 localStorage.setItem("_user", "guest");
 localStorage.setItem("_open_weather_map_apikey", "cdf61a76f093d1b3fc918e8cdd31bf4e");
 localStorage.setItem("_ajaxRequestInProgress", "false");
-//localStorage.setItem("_FB_app_id", "1631061333814819");
-localStorage.setItem("_FB_app_id", "1685220765065542");
+localStorage.setItem("_FB_app_id", "1631061333814819");
 
 
 if (localStorage.getItem("application_env") == "test") {
