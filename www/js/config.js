63ea0b7fbf40290576732ec373db548307b44fce
Jackie <jacquelyn.arroyo-tandang@ima-mt.com>
2016-08-03 15:54:38 +0800

    FB ID for PCAARD

--- www/js/config.js
+++ www/js/config.js
@@ -9,7 +9,7 @@ localStorage.setItem("_api_key", "456");
 localStorage.setItem("_user", "guest");
 localStorage.setItem("_open_weather_map_apikey", "cdf61a76f093d1b3fc918e8cdd31bf4e");
 localStorage.setItem("_ajaxRequestInProgress", "false");
-localStorage.setItem("_FB_app_id", "1631061333814819");
+localStorage.setItem("_FB_app_id", "493583617511613");
 localStorage.setItem("gps_enabled", "true");
 localStorage.setItem("application_name", "PCAARRD Alerts");
 localStorage.setItem("all_location_id", "1783");
@@ -18,7 +18,7 @@ localStorage.setItem("all_location_id", "1783");
 
 if (localStorage.getItem("application_env") == "test") {
     localStorage.setItem("_FB_app_id", "1685220765065542");
-    localStorage.setItem("_api_server", "http://localhost:82");
+    //localStorage.setItem("_api_server", "http://localhost:82");
     //localStorage.setItem("_api_server", "http://www.sarai.ph:82");
     //localStorage.setItem("_domain", "localhost:3000");
     localStorage.setItem("_domain", "http://www.phonegap-local.com:3000");
